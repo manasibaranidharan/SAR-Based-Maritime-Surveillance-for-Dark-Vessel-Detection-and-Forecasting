ef7b0ce5454e5b0c2c6b72c3d1a882174b187062
Manasi <manasibaranidharan@gmail.com>
2026-04-15 20:14:47 +0530

    tested model another time with different images

--- test_model.py
+++ test_model.py
@@ -5,7 +5,7 @@ model = YOLO("runs/detect/sar_ship_detector/weights/best.pt")
 
 # run prediction on one SAR patch
 results = model.predict(
-    source="processed_ai_dataset/images/train/ship_159.png",
+    source="processed_ai_dataset/images/train/ship_161.png",
     conf=0.05,
     save=True
 )
